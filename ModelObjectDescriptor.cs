63cc97cd6b0584480b30847b57d1c720185ec3ff
Shepherd Mike <pasztormiki@mail.com>
2025-04-14 21:49:36 +0300

    rendesen teglalapokbol rajzolja ki a hordokat
    helyesen

--- ModelObjectDescriptor.cs
+++ ModelObjectDescriptor.cs
@@ -1,7 +1,11 @@
 ﻿using Silk.NET.OpenGL;
+using Silk.NET.Vulkan;
 using System;
-using System.IO;
-using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace GrafikaSzeminarium
 {
@@ -15,51 +19,19 @@ namespace GrafikaSzeminarium
         public uint Indices { get; private set; }
         public uint IndexArrayLength { get; private set; }
 
-        public GL Gl { get; set; }
+        private GL Gl;
 
-        
-        public unsafe static ModelObjectDescriptor CreateCube(GL Gl)
+        public unsafe static ModelObjectDescriptor CreateCube(GL gl)
         {
-            uint vao = Gl.GenVertexArray();
-            Gl.BindVertexArray(vao);
+            uint vao = gl.GenVertexArray();
+            gl.BindVertexArray(vao);
 
             // counter clockwise is front facing
             float[] vertexArray = new float[] {
-                // top face
-                -0.5f, 0.5f, 0.5f, 0f, 1f, 0f,
-                0.5f, 0.5f, 0.5f, 0f, 1f, 0f,
-                0.5f, 0.5f, -0.5f, 0f, 1f, 0f,
-                -0.5f, 0.5f, -0.5f, 0f, 1f, 0f, 
-
-                // front face
-                -0.5f, 0.5f, 0.5f, 0f, 0f, 1f,
-                -0.5f, -0.5f, 0.5f, 0f, 0f, 1f,
-                0.5f, -0.5f, 0.5f, 0f, 0f, 1f,
-                0.5f, 0.5f, 0.5f, 0f, 0f, 1f,
-
-                // left face
-                -0.5f, 0.5f, 0.5f, -1f, 0f, 0f,
-                -0.5f, 0.5f, -0.5f, -1f, 0f, 0f,
-                -0.5f, -0.5f, -0.5f, -1f, 0f, 0f,
-                -0.5f, -0.5f, 0.5f, -1f, 0f, 0f,
-
-                // bottom face
-                -0.5f, -0.5f, 0.5f, 0f, -1f, 0f,
-                0.5f, -0.5f, 0.5f,0f, -1f, 0f,
-                0.5f, -0.5f, -0.5f,0f, -1f, 0f,
-                -0.5f, -0.5f, -0.5f,0f, -1f, 0f,
-
-                // back face
-                0.5f, 0.5f, -0.5f, 0f, 0f, -1f,
-                -0.5f, 0.5f, -0.5f,0f, 0f, -1f,
-                -0.5f, -0.5f, -0.5f,0f, 0f, -1f,
-                0.5f, -0.5f, -0.5f,0f, 0f, -1f,
-
-                // right face
-                0.5f, 0.5f, 0.5f, 1f, 0f, 0f,
-                0.5f, 0.5f, -0.5f,1f, 0f, 0f,
-                0.5f, -0.5f, -0.5f,1f, 0f, 0f,
-                0.5f, -0.5f, 0.5f,1f, 0f, 0f,
+                -0.5f, 1.0f, 0.5f, 0f, 0f, 1f,
+                -0.5f, -1.0f, 0.5f, 0f, 0f, 1f,
+                 0.5f, -1.0f, 0.5f, 0f, 0f, 1f,
+                 0.5f, 1.0f, 0.5f, 0f, 0f, 1f
             };
 
             float[] colorArray = new float[] {
@@ -67,171 +39,49 @@ namespace GrafikaSzeminarium
                 1.0f, 0.0f, 0.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
                 1.0f, 0.0f, 0.0f, 1.0f,
-
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-                0.0f, 1.0f, 0.0f, 1.0f,
-
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-                0.0f, 0.0f, 1.0f, 1.0f,
-
-                1.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, 0.0f, 1.0f, 1.0f,
-                1.0f, 0.0f, 1.0f, 1.0f,
-
-                0.0f, 1.0f, 1.0f, 1.0f,
-                0.0f, 1.0f, 1.0f, 1.0f,
-                0.0f, 1.0f, 1.0f, 1.0f,
-                0.0f, 1.0f, 1.0f, 1.0f,
-
-                1.0f, 1.0f, 0.0f, 1.0f,
-                1.0f, 1.0f, 0.0f, 1.0f,
-                1.0f, 1.0f, 0.0f, 1.0f,
-                1.0f, 1.0f, 0.0f, 1.0f,
             };
 
             uint[] indexArray = new uint[] {
                 0, 1, 2,
                 0, 2, 3,
-
-                4, 5, 6,
-                4, 6, 7,
-
-                8, 9, 10,
-                10, 11, 8,
-
-                12, 14, 13,
-                12, 15, 14,
-
-                17, 16, 19,
-                17, 19, 18,
-
-                20, 22, 21,
-                20, 23, 22
+                2, 1, 0,
+                3, 2, 0
             };
 
-            uint vertices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
+            uint vertices = gl.GenBuffer();
+            gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+            gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
             // 0 is position; 2 is normals
             uint offsetPos = 0;
             uint offsetNormals = offsetPos + 3 * sizeof(float);
             uint vertexSize = offsetNormals + 3 * sizeof(float);
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetPos);
-            Gl.EnableVertexAttribArray(0);
-            Gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, true, vertexSize, (void*)offsetNormals);
-            Gl.EnableVertexAttribArray(2);
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+            gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetPos);
+            gl.EnableVertexAttribArray(0);
+            gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, true, vertexSize, (void*)offsetNormals);
+            gl.EnableVertexAttribArray(2);
+            gl.BindBuffer(GLEnum.ArrayBuffer, 0);
 
-            uint colors = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
+            uint colors = gl.GenBuffer();
+            gl.BindBuffer(GLEnum.ArrayBuffer, colors);
+            gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
             // 1 is color
-            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+            gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
+            gl.EnableVertexAttribArray(1);
+            gl.BindBuffer(GLEnum.ArrayBuffer, 0);
 
-            uint indices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
-            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+            uint indices = gl.GenBuffer();
+            gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
+            gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
+            gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
 
-            return new ModelObjectDescriptor() { Vao = vao, Vertices = vertices, Colors = colors, Indices = indices, IndexArrayLength = (uint)indexArray.Length, Gl = Gl };
-        }
-
-        // uj metordus teglalaphoz
-        public unsafe static ModelObjectDescriptor CreateRectangle(GL Gl)
-        {
-            // generalj VAO t a teglalaphoz
-            uint vao = Gl.GenVertexArray();
-            Gl.BindVertexArray(vao);
-
-            
-            // kozeppont 0 0 
-            float[] vertexArray = new float[]
-            {
-                -0.5f, -1.0f, 0f,   // bal also
-                 0.5f, -1.0f, 0f,   // jobb also
-                 0.5f,  1.0f, 0f,   // jobb felso
-                -0.5f,  1.0f, 0f    // bal felso
-            };
-
-            // a normal minden csucshoz 0 0 1 mert sikban fekszenek
-            float[] normalArray = new float[]
-            {
-                0f, 0f, 1f,
-                0f, 0f, 1f,
-                0f, 0f, 1f,
-                0f, 0f, 1f,
-            };
-
-            // minden szin piros
-            float[] colorArray = new float[]
-            {
-                1f, 0f, 0f, 1f,
-                1f, 0f, 0f, 1f,
-                1f, 0f, 0f, 1f,
-                1f, 0f, 0f, 1f,
-            };
-
-            // indexek a 2 haromszoghoz
-            uint[] indexArray = new uint[]
-            {
-                0, 1, 2,
-                0, 2, 3
-            };
-
-            uint vertices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
-            // keszitsunk vegyes tombot atributumok es normalok
-            float[] vertexData = new float[4 * 6]; // 4 csucs 6 ertek
-            for (int i = 0; i < 4; i++)
-            {
-                vertexData[i * 6 + 0] = vertexArray[i * 3 + 0]; // x
-                vertexData[i * 6 + 1] = vertexArray[i * 3 + 1]; // y
-                vertexData[i * 6 + 2] = vertexArray[i * 3 + 2]; // z
-                vertexData[i * 6 + 3] = normalArray[i * 3 + 0]; // norm x
-                vertexData[i * 6 + 4] = normalArray[i * 3 + 1]; // norm y
-                vertexData[i * 6 + 5] = normalArray[i * 3 + 2]; // norm z
-            }
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexData.AsSpan(), GLEnum.StaticDraw);
-            // attributumok 0 pozicio 2 normal
-            uint offsetPos = 0;
-            uint offsetNormals = (uint)(3 * sizeof(float));
-            uint vertexSize = (uint)(6 * sizeof(float));
-            Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetPos);
-            Gl.EnableVertexAttribArray(0);
-            Gl.VertexAttribPointer(2, 3, VertexAttribPointerType.Float, false, vertexSize, (void*)offsetNormals);
-            Gl.EnableVertexAttribArray(2);
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
-
-            // kulon szin buffer letrehozasa
-            uint colors = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ArrayBuffer, colors);
-            Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)colorArray.AsSpan(), GLEnum.StaticDraw);
-            
-            // 1-es atributum szinekhez
-            Gl.VertexAttribPointer(1, 4, VertexAttribPointerType.Float, false, 0, null);
-            Gl.EnableVertexAttribArray(1);
-            Gl.BindBuffer(GLEnum.ArrayBuffer, 0);
-
-            uint indices = Gl.GenBuffer();
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, indices);
-            Gl.BufferData(GLEnum.ElementArrayBuffer, (ReadOnlySpan<uint>)indexArray.AsSpan(), GLEnum.StaticDraw);
-            Gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
-
-            return new ModelObjectDescriptor()
-            {
-                Vao = vao,
-                Vertices = vertices,
-                Colors = colors,
-                Indices = indices,
-                IndexArrayLength = (uint)indexArray.Length,
-                Gl = Gl
+            return new ModelObjectDescriptor() 
+            { 
+                Vao = vao, 
+                Vertices = vertices, 
+                Colors = colors, 
+                Indices = indices, 
+                IndexArrayLength = (uint)indexArray.Length, 
+                Gl = gl 
             };
         }
 
@@ -241,7 +91,7 @@ namespace GrafikaSzeminarium
             {
                 if (disposing)
                 {
-                    // TODO: dispose managed state (managed objects)
+                    // Dispose managed state if needed
                 }
 
                 Gl.DeleteBuffer(Vertices);
--- obj/Debug/net8.0/GrafikaSzeminarium.AssemblyInfo.cs
+++ obj/Debug/net8.0/GrafikaSzeminarium.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GrafikaSzeminarium")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde1765209c8318d757ad04d09717eb3def0b0f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd861f1658acc302fc2958da1e67201e9dc83adc")]
 [assembly: System.Reflection.AssemblyProductAttribute("GrafikaSzeminarium")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GrafikaSzeminarium")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
